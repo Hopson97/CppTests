c58085de5f621c418b8fb3827e6ccb1363a37b43
Hopson97 <mhopson@hotmail.co.uk>
2018-03-27 08:24:52 +0100

    boxes inside of boxes test

--- SFGUITest/src/Main.cpp
+++ SFGUITest/src/Main.cpp
@@ -6,9 +6,13 @@
 
 #include <SFML/Graphics.hpp>
 
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFGUI Buttons Example", sf::Style::Titlebar | sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(1280, 768), "SFGUI Buttons Example", sf::Style::Titlebar | sf::Style::Close);
+
+   
 
 
     window.resetGLStates();
@@ -20,22 +24,26 @@ int main()
     auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.f);
     auto scrollBox = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.f);
 
+    sf::Image img;
+    img.loadFromFile("res/btn.png");
+    for (int i = 0; i < 10; i++) {
+        auto box = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL, 5.f);
+        for (int i = 0; i < 10; i++) {
+            auto b = sfg::Button::Create();
+            b->SetImage(sfg::Image::Create(img));
+            box->Pack(b);
+        }
+        scrollBox->Pack(box);
+    }
+
     auto scroll = sfg::ScrolledWindow::Create();
     scroll->SetScrollbarPolicy(sfg::ScrolledWindow::ScrollbarPolicy::HORIZONTAL_ALWAYS |
         sfg::ScrolledWindow::ScrollbarPolicy::VERTICAL_ALWAYS);
 
-    scroll->SetRequisition(sf::Vector2f(500.f, 100.f));
+    scroll->SetRequisition(sf::Vector2f(100.f, 100.f));
     scroll->AddWithViewport(scrollBox);
     box->Pack(scroll);
     guiWindow->Add(box);
-
-    sf::Image img;
-    img.loadFromFile("res/tiles.png");
-    for (int i = 0; i < 10; i++) {
-        auto b = sfg::Button::Create("SWAG");
-        b->SetImage(sfg::Image::Create(img));
-        scrollBox->Pack(b);
-    }
 
     while (window.isOpen()) {
         sf::Event event;
