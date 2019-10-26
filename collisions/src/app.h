95b10d5d44e9d571ba9be0c4a31ac77617a80c4f
Hopson97 <mhopson@hotmail.co.uk>
2019-10-26 22:17:18 +0100

    Add objects and structs to the app header

--- collisions/src/app.h
+++ collisions/src/app.h
@@ -5,9 +5,32 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <vector>
 
+#include "input/keyboard.h"
+
 class Application {
+  struct Tile {
+    int type;
+    int flag;
+  };
+
+  struct Entity {
+    sf::RectangleShape sprite;
+    sf::Vector2f velocity;
+  };
+
   public:
     Application();
 
+    void onEvent(sf::Event e);
+    void onInput();
+    void onUpdate();
+    void onRender();
+
   private:
-}
+    sf::RectangleShape m_tile;
+    sf::RenderWindow m_window;
+    sf::Texture m_playerTexture;
+    
+    Keyboard m_keyboard;
+    Entity m_player;
+};
